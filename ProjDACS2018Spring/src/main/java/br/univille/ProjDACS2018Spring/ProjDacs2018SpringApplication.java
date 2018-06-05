3141ab063820cfb5add5d1327a76d7c2870a44a5
MARCELO ALVES JUNIOR <marcelo.junior.1@univille.br>
2018-06-04 21:35:18 -0300

    Second Commit

---
+++ ProjDACS2018Spring/src/main/java/br/univille/ProjDACS2018Spring/ProjDacs2018SpringApplication.java
@@ -0,0 +1,12 @@
+package br.univille.ProjDACS2018Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDacs2018SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDacs2018SpringApplication.class, args);
+	}
+}
